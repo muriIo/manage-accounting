f1138430880de3f42a6da7b317d9515fd58a3dab
Murilo Mendes da Cruz <murilomecr@gmail.com->
2024-04-29 22:42:12 -0300

    adding app routing to organize routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +10,7 @@ import { RouterModule } from '@angular/router';
   ],
   imports: [
     BrowserModule,
-    RouterModule
+    AppRoutingModule
   ],
   bootstrap: [AppComponent]
 })
